dbba15ddfe7022d8aa82db1b2f61cdc255255ae5
Prathambegale <71150422+Prathambegale@users.noreply.github.com>
2020-09-25 02:16:44 +0530

    Add files via upload

---
+++ function recrtion sum of digits.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+addition();
+main()
+{
+    addition();
+}
+addition()
+
+{
+    int a,b,c,d;
+    printf("enter the numder of digits");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    printf("\nthe sum of the digits is %d",a+b+c+d);
+}
